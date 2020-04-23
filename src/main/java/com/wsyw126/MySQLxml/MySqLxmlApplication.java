a7b47aa96f54b6b9ebe46daf86e2f49c22a82aca
杨大 <yjp138557@antfin.com>
2020-04-23 14:37:56 +0800

    init project

---
+++ src/main/java/com/wsyw126/MySQLxml/MySqLxmlApplication.java
@@ -0,0 +1,13 @@
+package com.wsyw126.MySQLxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqLxmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqLxmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsyw126/MySQLxml/MySqLxmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsyw126.MySQLxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqLxmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
