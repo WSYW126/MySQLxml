0a24477674d058556809a7cc88def47ae883baef
杨大 <yjp138557@antfin.com>
2021-03-14 23:20:56 +0800

    冲突

--- src/main/java/com/wsyw126/MySQLxml/MySqLxmlApplication.java
+++ src/main/java/com/wsyw126/MySQLxml/MySqLxmlApplication.java
@@ -9,7 +9,7 @@ public class MySqLxmlApplication {
     public static void main(String[] args) {
         SpringApplication.run(MySqLxmlApplication.class, args);
         //测试不冲突
-        //测试---冲突 啊
+        //测试冲突
     }
     //测试不冲突
     //测试---冲突 啊
